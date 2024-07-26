9649e44bd6f2bf6aeaecfe3e78af18db43c7c77f
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-07-26 13:35:06 -0300

    reorganizando o pre processamento e entendendo melhor o escalonamento de valores.

---
+++ credito/credito.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+from sklearn.preprocessing import StandardScaler # calculo de padronização
+
+#========= Exploração dos dados =========
+df=pd.read_csv('credit_data.csv')
+
+# ========= Visualização dos dados =========
+np.unique(df['default'], return_counts=True)
+sns.countplot(x=df['default'],)
+plt.hist(x=df['age'])
+plt.hist(x=df['income'])
+plt.hist(x=df['loan'])
+grafico=px.scatter_matrix(df,dimensions=['age','income','loan'],color='default')
+
+#========= tratamento de valores incosistentes =========
+df[df['age'] <0]
+
+# Podemos apagar a coluna inteira
+df2=df.drop('age',axis=1) # axis = 1 = coluna
+
+# Podemos apagar somente os registos com valores incosistentes
+df3= df.drop(df[df['age'] < 0].index)
+df3[df3['age'] < 0]
+
+# Podemos preencher os valores manualmente
+df['age'].mean()
+df[df['age'] > 0]['age'].mean()
+df[df['age'] < 0]= df[df['age'] > 0]['age'].mean()
+df['age'].mean()
+
+#========= tratamento de valores incosistentes =========
+df.isna().sum()
+df.loc[pd.isna(df['age'])]
+df['age'].fillna(df['age'].mean(),inplace=True)
+
+#========= divisao entre previsores e classes =========
+X= df.iloc[:,1:4].values
+y= df.iloc[:,-1].values
+
+#========= escalonamento de valores =========
+scaler = StandardScaler()
+X=scaler.fit_transform(X)
+
